4901c954007bb3726b76b1989367b281eaa07e3f
John B <john@nexys.ch>
2019-10-09 12:22:21 +0200

    added build

---
+++ build/precache-manifest.80233b39daca4d4f3cb1bad3403b469e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f707719ff094cd21cf019b5d49f812d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b94e4ebecd07f30348f",
+    "url": "/static/js/2.bb436385.chunk.js"
+  },
+  {
+    "revision": "54529503fe26c3ed7a22",
+    "url": "/static/js/main.0b8087a7.chunk.js"
+  },
+  {
+    "revision": "cca601e954b42df9e2c4",
+    "url": "/static/js/runtime-main.bbdc7b2a.js"
+  }
+]);
